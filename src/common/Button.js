4330f3dc00da6467081ea740261cc45190c66d19
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2022-04-21 15:02:41 -0700

    intro page structuring

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function StartButton(props) {
+export default function Button(props) {
   return (
 <button>{props.text}</button>
   )
--- src/start/Intro.js
+++ src/start/Intro.js
@@ -3,9 +3,14 @@ import Logo from './Logo.js'
 import Button from '../common/Button.js'
 
 export default function Start() {
+  let buttonText = "Start"
+  const handleClick () => {
+
+  }
   return (
     <div>
       <Logo/>
+      <Button onClick={handleClick} text={buttonText}></Button>
     </div>
   )
 }
